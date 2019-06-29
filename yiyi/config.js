39c10b45be937f3001b38aa42cdf4d03558bb691
wushang941018 <liuliang941018@foxmail.com>
2019-06-29 09:44:06 +0800

    生产环境和开发环境的数据库配置

--- yiyi/config.js
+++ yiyi/config.js
@@ -1,12 +1,26 @@
 const path = require('path');
-module.exports = {
+const DEV_CONFIG = {
+    DB_HOST: "localhost",
+    DB_PORT: 3306,
+    DB_USER: "root",
+    DB_PASS: "123456",
+    DB_NAME: "yiyi"
+};
+const PRO_CONFIG = {
     // 连接数据库信息
     DB_HOST: "172.17.0.9",
     DB_PORT: 3306,
     DB_USER: "yiyi",
     DB_PASS: "shenzhenyiyi",
-    DB_NAME: "yiyi",
+    DB_NAME: "yiyi"
+};
 
+const IS_WINDOWS = process.env.SystemRoot.toLowerCase().includes('windows');
+
+const DB_CONFIG = IS_WINDOWS ? DEV_CONFIG : PRO_CONFIG;
+
+module.exports = {
+    ...DB_CONFIG,
     // 服务器监听端口
     PORT: 5666,
 
